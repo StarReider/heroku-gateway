bfe7f0299d7f00f5993f239681138d5c57e16e99
bezugomonnov <bezugomonnov@WSM-0768.netcracker.com>
2019-04-12 19:04:01 +0300

    CORS support

--- src/main/java/microservices/book/gateway/configuration/WebSecurityConfig.java
+++ src/main/java/microservices/book/gateway/configuration/WebSecurityConfig.java
@@ -16,15 +16,17 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().requiresChannel()
-	      .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-	      .requiresSecure();
+		http.cors().and()
+			//if X-Forwarded-Proto then use https (heroku)
+		    .requiresChannel()
+	        .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+	        .requiresSecure();
 	}
 	
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowCredentials(true);
+		//configuration.setAllowCredentials(true);
 		configuration.setAllowedOrigins(Arrays.asList(CorsConfiguration.ALL));
 		configuration.setAllowedMethods(Arrays.asList(HttpMethod.GET.name(), HttpMethod.OPTIONS.name(), HttpMethod.POST.name()));
 		configuration.setAllowedHeaders(Arrays.asList(CorsConfiguration.ALL));
