f6e56dca29229b7ced696f3f59c1e4b6fc261f97
bezugomonnov <bezugomonnov@WSM-0768.netcracker.com>
2019-04-12 17:34:54 +0300

    cors support

--- src/main/java/microservices/book/gateway/configuration/WebSecurityConfig.java
+++ src/main/java/microservices/book/gateway/configuration/WebSecurityConfig.java
@@ -1,15 +1,35 @@
 package microservices.book.gateway.configuration;
 
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.requiresChannel()
+		http.cors().and().requiresChannel()
 	      .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
 	      .requiresSecure();
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Collections.singletonList(CorsConfiguration.ALL));
+		configuration.setAllowedMethods(Collections.singletonList(CorsConfiguration.ALL));
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		
+		return source;
 	}
 }
